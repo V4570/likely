6bf5934edbd2ece829fd920925c4224598fecd72
v4570 <vasilis.tos@gmail.com>
2021-05-09 21:26:03 +0300

    Updates: Password validation.

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -31,14 +31,44 @@
                   </b-input>
                 </b-field>
 
-                <b-field label="Password" label-position="on-border">
+                <b-field label="Password"
+                         label-position="on-border"
+                         :type="{'is-warning': !passwordValidate, 'is-success': passwordValidate}"
+                         :message="{
+                          'Minimum 8 characters': !this.pEightMatch,
+                          'One lowercase letter': !this.pLowerMatch,
+                          'One uppercase letter': !this.pUpperMatch,
+                          'One digit': !this.pNumMatch,
+                          'One special character': !this.pSpecialMatch,
+                          'No white spaces': !this.pWhiteMatch
+                        }"
+                         class="has-text-left">
                   <b-input
                       type="password"
                       v-model="password"
+                      @input="validatePassword"
                       password-reveal
                       placeholder="Your password"
                       required
-                      icon="lock">
+                      icon="lock"
+                      maxlength="30">
+                  </b-input>
+                </b-field>
+
+                <b-field label="Repeat Password"
+                         label-position="on-border"
+                         :type="{'is-danger': !passwordsMatch, 'is-success': passwordsMatch}"
+                         :message="{'Passwords do not match': !passwordsMatch}"
+                         class="has-text-left">
+                  <b-input
+                      type="password"
+                      v-model="password2"
+                      @input="checkPasswords"
+                      password-reveal
+                      placeholder="Your password"
+                      required
+                      icon="lock"
+                      maxlength="30">
                   </b-input>
                 </b-field>
                 <div class="mt-5">
@@ -64,6 +94,8 @@
 
 <script>
 import { SIGNUP_REQUEST } from "@/store"
+import { isEmpty } from 'lodash'
+import { helpers } from 'vuelidate/lib/validators'
 
 export default {
   name: "Signup",
@@ -71,27 +103,94 @@ export default {
     return {
       username: '',
       email: '',
-      password: ''
+      password: '',
+      password2: '',
+      passwordsMatch: false,
+      passwordValidate: false,
+
+      passwordNumberValidator: helpers.regex('p_num', /^(?=.*\d)/),
+      pNumMatch: false,
+
+      passwordLowercaseValidator: helpers.regex('p_lower', /^(?=.*[a-z])/),
+      pLowerMatch: false,
+
+      passwordUppercaseValidator: helpers.regex('p_upper', /^(?=.*[A-Z])/),
+      pUpperMatch: false,
+
+      passwordSpecialValidator: helpers.regex('p_special', /^(?=.*[!#$%&? "])/),
+      pSpecialMatch: false,
+
+      passwordNoWhiteSpaceValidator: helpers.regex('p_white', /^(?!.*\s)/),
+      pWhiteMatch: false,
+
+      passwordAtLeastEightValidator: helpers.regex('p_eight', /.{8,}$/),
+      pEightMatch: false
     }
   },
   methods: {
-    signup () {
-      this.$store.dispatch(SIGNUP_REQUEST,
-          {username: this.username, password: this.password, email: this.email}).then(() =>{
-        this.$router.push('/login')
-      }).catch(e => {
-        const errorResponse = e.response
+    validatePassword(){
 
-        switch (errorResponse.status) {
-          case 409:
-            this.alertCustomError("Conflict Error",
-                "A user with this username or email already exists.<br/>" +
-                "Try Logging in or use another username and email.")
-            break
-          default:
-            console.log(e)
-        }
-      })
+      if(!isEmpty(this.password)){
+        this.pEightMatch = !!this.passwordAtLeastEightValidator(this.password)
+        this.pWhiteMatch = !!this.passwordNoWhiteSpaceValidator(this.password)
+        this.pSpecialMatch = !!this.passwordSpecialValidator(this.password)
+        this.pUpperMatch = !!this.passwordUppercaseValidator(this.password)
+        this.pLowerMatch = !!this.passwordLowercaseValidator(this.password)
+        this.pNumMatch = !!this.passwordNumberValidator(this.password)
+
+        this.passwordValidate = !!(
+            this.pEightMatch && this.pWhiteMatch &&
+            this.pSpecialMatch && this.pUpperMatch &&
+            this.pLowerMatch && this.pNumMatch)
+      }
+      else{
+        this.pEightMatch = false
+        this.pWhiteMatch = false
+        this.pSpecialMatch = false
+        this.pUpperMatch = false
+        this.pLowerMatch = false
+        this.pNumMatch = false
+        this.passwordValidate = false
+      }
+    },
+    checkPasswords() {
+      if(isEmpty(this.password) || isEmpty(this.password2)){
+        this.passwordsMatch = false
+      }else{
+        this.passwordsMatch = (this.password === this.password2)
+      }
+    },
+    signup () {
+      if(isEmpty(this.password) || isEmpty(this.username) || isEmpty(this.email)){
+        this.alertCustomError("Empty Fields",
+            "All fields are required to signup.")
+      }
+      else{
+        this.$store.dispatch(SIGNUP_REQUEST,
+            {username: this.username, password: this.password, email: this.email}).then(() =>{
+          this.$router.push('/login')
+
+          const toastMessage = `User ${this.username} created successfully.`
+
+          this.$buefy.toast.open({
+            duration: 3000,
+            message: toastMessage,
+            type: 'is-success'
+          })
+        }).catch(e => {
+          const errorResponse = e.response
+
+          switch (errorResponse.status) {
+            case 409:
+              this.alertCustomError("Conflict Error",
+                  "A user with this username or email already exists.<br/>" +
+                  "Try Logging in or use another username and email.")
+              break
+            default:
+              console.log(e)
+          }
+        })
+      }
     },
     alertCustomError(errorTitle, msg) {
       this.$buefy.dialog.alert({
